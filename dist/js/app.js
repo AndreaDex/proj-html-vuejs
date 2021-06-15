01b3ca6b60595de87d97ba1326d773a3ac45d2d3
AndreaDex <desiderio.andrea@outlook.it>
2021-06-15 17:57:46 +0200

    complete moment section

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,20 @@
 var app = new Vue({
   el: "#app",
   data: {
-    menuLinks: ["home", "mission", "causes", "journal", "donate"]
+    menuLinks: ["home", "mission", "causes", "journal", "donate"],
+    liveStats: [{
+      icon: "far fa-heart",
+      stat: 2032,
+      desc: "Volunteers Worldeide"
+    }, {
+      icon: "fas fa-globe",
+      stat: 132,
+      desc: "Active projects"
+    }, {
+      icon: "fas fa-dollar-sign",
+      stat: "3.8M",
+      desc: "Donated"
+    }]
   },
   methods: {}
 });
--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@ const app = new Vue({
   el: "#app",
   data: {
     menuLinks: ["home", "mission", "causes", "journal", "donate"],
+    liveStats: [
+      {
+        icon: "far fa-heart",
+        stat: 2032,
+        desc: "Volunteers Worldeide",
+      },
+      {
+        icon: "fas fa-globe",
+        stat: 132,
+        desc: "Active projects",
+      },
+      {
+        icon: "fas fa-dollar-sign",
+        stat: "3.8M",
+        desc: "Donated",
+      },
+    ],
   },
   methods: {},
 });
