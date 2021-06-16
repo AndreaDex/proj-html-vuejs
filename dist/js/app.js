0f6284ea2b6a854138820b4994972fe0ba4d7b3d
AndreaDex <desiderio.andrea@outlook.it>
2021-06-16 12:02:33 +0200

    create article objs in app.js

--- dist/js/app.js
+++ dist/js/app.js
@@ -40,6 +40,27 @@ var app = new Vue({
       icon: "fab fa-pagelines",
       title: "Farming",
       text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit, quidem ratione minus ipsa iste voluptate."
+    }],
+    articles: [{
+      image: "./dist/img/photo-1444213007800-cff19e1677ac.jpg",
+      title: "Understanding community complexities",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum. Minima quos sunt fugit"
+    }, {
+      image: "./dist/img/photo-1444664597500-035db93e2323-177x142.jpg",
+      title: "The human story of uniqueness",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum. Minima quos sunt fugit"
+    }, {
+      image: "./dist/img/photo-1447430617419-95715602278e-177x142.jpg",
+      title: "Sustainable trade tactis",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum. Minima quos sunt fugit"
+    }, {
+      image: "./dist/img/photo-1460230525622-630fe3294cd7-177x142.jpg",
+      title: "Farmers making a difference",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum. Minima quos sunt fugit"
+    }, {
+      image: "./dist/img/photo-1460600421604-5e138c208b9c-177x142.jpg",
+      title: "Meeting remote tribes in Perù",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum. Minima quos sunt fugit"
     }]
   },
   methods: {}
