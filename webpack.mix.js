3f556c08f6c1a30f4182ae89475584dabd1acf9f
AndreaDex <desiderio.andrea@outlook.it>
2021-06-16 17:50:41 +0200

    fix background position bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,9 @@ let mix = require("laravel-mix");
 
 mix
   .js("src/app.js", "js")
-  .sass("src/app.scss", "css", {
-    processUrls: false,
+  .sass("src/app.scss", "css")
+  .options({
+    processCssUrls: false,
   })
   .copyDirectory("src/img", "dist/img")
   .setPublicPath("dist");
